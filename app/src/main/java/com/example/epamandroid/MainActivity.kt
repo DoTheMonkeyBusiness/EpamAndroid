dc4134709a538a6c14858c64b794f37acafa5c20
Max Nasalevich <maks20061999@gmail.com>
2019-02-23 21:25:30 +0300

    add button onClick fun in feature_2

--- app/src/main/java/com/example/epamandroid/MainActivity.kt
+++ app/src/main/java/com/example/epamandroid/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.epamandroid
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 
 /**
  *
  */
 class MainActivity : AppCompatActivity() {
 
+
+    private val button1: Button = findViewById(R.id.button1)
     /**
      *
      */
@@ -16,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("Error", "msg")
+
+    }
+
+
+    fun onButtonClick(view: View) {
+        button1.setText(R.string.button1_new)
     }
 }
