8feddce8a9cc01de2f5b3024dfe7bed58eb5baa8
Max Nasalevich <maks20061999@gmail.com>
2019-02-27 14:31:13 +0300

    using kotlin extensions

--- app/src/main/java/com/example/epamandroid/MainActivity.kt
+++ app/src/main/java/com/example/epamandroid/MainActivity.kt
@@ -4,8 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import com.example.mysubmodule.Myclass
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  *
@@ -13,12 +13,10 @@ import com.example.mysubmodule.Myclass
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var button1: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button1 = findViewById(R.id.button1)
         Myclass.myFunc()
     }
 
