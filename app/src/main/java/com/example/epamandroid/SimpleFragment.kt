64c0d0a3cfff227a57d96667dc6e9ae8ce6614b4
Max Nasalevich <maks20061999@gmail.com>
2019-03-06 15:17:54 +0300

    change const

--- app/src/main/java/com/example/epamandroid/SimpleFragment.kt
+++ app/src/main/java/com/example/epamandroid/SimpleFragment.kt
@@ -7,9 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.simple_fragment.simple_fragment_hello_button
 
-class SimpleFragment : Fragment() {
+private const val BUTTON_TEXT: String = "buttonText"
 
-    private val BUTTON_TEXT: String = "buttonText"
+class SimpleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.simple_fragment, container, false)
