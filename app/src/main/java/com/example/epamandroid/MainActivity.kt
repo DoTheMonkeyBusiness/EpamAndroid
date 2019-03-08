bfd5fb71d330db5126d49e346e4c301d5a8ffc57
Max Nasalevich <maks20061999@gmail.com>
2019-03-08 23:21:02 +0300

    add toolbar

--- app/src/main/java/com/example/epamandroid/MainActivity.kt
+++ app/src/main/java/com/example/epamandroid/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.epamandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mysubmodule.Myclass
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.my_toolbar
 
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Myclass.myFunc()
+        setSupportActionBar(my_toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
