7d12ffdb23f5985740e1278423516aae1bee30b4
Max Nasalevich <maks20061999@gmail.com>
2019-03-08 16:50:06 +0300

    change service state constant

--- app/src/main/java/com/example/epamandroid/Constants.kt
+++ app/src/main/java/com/example/epamandroid/Constants.kt
@@ -2,6 +2,6 @@ package com.example.epamandroid
 
 object Constants {
     const val CUSTOM_ACTION: String = "com.example.epamandroid.myAction"
-    const val SERVICE_STATE: String = "timeNow"
+    const val SERVICE_STATE: String = "serviceState"
     const val BROADCAST_MESSAGE: String = "broadcastMessage"
 }
