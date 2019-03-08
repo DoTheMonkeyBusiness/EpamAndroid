746ea2a7ae56025b8613bf2bcc87cb7f8937ad10
Max Nasalevich <maks20061999@gmail.com>
2019-03-08 19:53:56 +0300

    clear master branch

--- app/src/main/java/com/example/epamandroid/MainActivity.kt
+++ app/src/main/java/com/example/epamandroid/MainActivity.kt
@@ -2,14 +2,8 @@ package com.example.epamandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import com.example.mysubmodule.Myclass
-import kotlinx.android.synthetic.main.activity_main.*
 
-/**
- *
- */
 class MainActivity : AppCompatActivity() {
 
 
@@ -18,10 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Myclass.myFunc()
-    }
-
-
-    fun onButtonClick(view: View) {
-        button1.setText(R.string.button1_new)
     }
 }
