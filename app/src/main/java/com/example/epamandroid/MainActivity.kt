6d052c8b5ecf1fd827226c4ba9559da5d2835564
Max Nasalevich <maks20061999@gmail.com>
2019-03-10 21:35:25 +0300

    resolve conflicts in main

--- app/src/main/java/com/example/epamandroid/MainActivity.kt
+++ app/src/main/java/com/example/epamandroid/MainActivity.kt
@@ -2,15 +2,11 @@ package com.example.epamandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mysubmodule.Myclass
 
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Myclass.myFunc()
     }
 }
