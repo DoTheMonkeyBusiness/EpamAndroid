97528626f99a2d1beefcc0f1f8aa4de7179773b2
Max Nasalevich <maks20061999@gmail.com>
2019-03-10 21:32:52 +0300

    resolve conflicts

--- app/src/main/java/com/example/epamandroid/MainActivity.kt
+++ app/src/main/java/com/example/epamandroid/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.epamandroid
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mysubmodule.Myclass
 import kotlinx.android.synthetic.main.activity_main.button1
 import kotlinx.android.synthetic.main.activity_main.second_activity_button
 
@@ -13,8 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Myclass.myFunc()
-
         button1.setOnClickListener {
             button1.setText(R.string.button1_new)
         }
