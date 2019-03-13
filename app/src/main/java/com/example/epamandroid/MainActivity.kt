f61facfe5e9b20781d97bc1650dc9a7ee88caf75
Max Nasalevich <maks20061999@gmail.com>
2019-03-13 15:41:43 +0300

    add custom toolbar

--- app/src/main/java/com/example/epamandroid/MainActivity.kt
+++ app/src/main/java/com/example/epamandroid/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.epamandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.activity_main_toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(activity_main_toolbar)
+
     }
 }
