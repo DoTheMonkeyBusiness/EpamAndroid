361d5cc3c32aa70a11c783b044344e44375179c6
Max Nasalevich <maks20061999@gmail.com>
2019-03-13 15:55:05 +0300

    add menu button

--- app/src/main/java/com/example/epamandroid/MainActivity.kt
+++ app/src/main/java/com/example/epamandroid/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.epamandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.activity_main_toolbar
+import kotlinx.android.synthetic.main.activity_main.activity_main_drawer_layout
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(activity_main_toolbar)
+        supportActionBar.apply {
+            this!!.setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp)
+        }
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                activity_main_drawer_layout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
