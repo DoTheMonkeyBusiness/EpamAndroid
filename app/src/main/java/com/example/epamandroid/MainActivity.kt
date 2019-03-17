cad9ee7b99af940d026be465468ff9a122041e03
Max Nasalevich <maks20061999@gmail.com>
2019-03-17 17:40:18 +0300

    removed useless button

--- app/src/main/java/com/example/epamandroid/MainActivity.kt
+++ app/src/main/java/com/example/epamandroid/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.epamandroid
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.button1
 import kotlinx.android.synthetic.main.activity_main.second_activity_button
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button1.setOnClickListener {
-            button1.setText(R.string.button1_new)
-        }
+
         second_activity_button.setOnClickListener {
             startActivity(Intent(this, SecondActivity::class.java))
         }
