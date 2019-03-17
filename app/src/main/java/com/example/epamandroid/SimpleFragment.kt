7db6c6cec8b464eea60fd7c86ddda478abd2a192
Max Nasalevich <maks20061999@gmail.com>
2019-03-17 17:49:26 +0300

    fixed to codestyle and add companion object

--- app/src/main/java/com/example/epamandroid/SimpleFragment.kt
+++ app/src/main/java/com/example/epamandroid/SimpleFragment.kt
@@ -7,9 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.simple_fragment.simple_fragment_hello_button
 
-private const val BUTTON_TEXT: String = "buttonText"
-
 class SimpleFragment : Fragment() {
+
+    companion object {
+        private const val BUTTON_TEXT_KEY: String = "buttonText"
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.simple_fragment, container, false)
@@ -19,7 +21,7 @@ class SimpleFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         if (savedInstanceState != null) {
-            simple_fragment_hello_button.text = savedInstanceState.getString(BUTTON_TEXT)
+            simple_fragment_hello_button.text = savedInstanceState.getString(BUTTON_TEXT_KEY)
         }
 
         simple_fragment_hello_button.setOnClickListener {
@@ -34,7 +36,7 @@ class SimpleFragment : Fragment() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
-        outState.putString(BUTTON_TEXT, simple_fragment_hello_button.text.toString())
+        outState.putString(BUTTON_TEXT_KEY, simple_fragment_hello_button.text.toString())
     }
 }
 
