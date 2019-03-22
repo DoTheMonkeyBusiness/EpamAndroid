3e4efada2d15a1e475328174c5fc524d6c22a365
Max Nasalevich <maks20061999@gmail.com>
2019-03-22 19:42:54 +0300

    undone changes

--- app/src/main/java/com/example/epamandroid/NavigationHeaderView.kt
+++ app/src/main/java/com/example/epamandroid/NavigationHeaderView.kt
@@ -7,9 +7,9 @@ import android.widget.RelativeLayout
 import kotlinx.android.synthetic.main.navigation_header_view.view.*
 
 class NavigationHeaderView @JvmOverloads constructor(
-        context: Context,
-        attrs: AttributeSet? = null,
-        defStyleAttr: Int = 0
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
 ) : RelativeLayout(context, attrs, defStyleAttr) {
     init {
         inflate(getContext(), R.layout.navigation_header_view, this)
@@ -17,9 +17,9 @@ class NavigationHeaderView @JvmOverloads constructor(
 
     fun updateIconColor(colorHex: String?) {
         navigationHeaderAndroidIcon
-                .setColorFilter(
-                        Color
-                                .parseColor(colorHex)
-                )
+            .setColorFilter(
+                Color
+                    .parseColor(colorHex)
+            )
     }
 }
