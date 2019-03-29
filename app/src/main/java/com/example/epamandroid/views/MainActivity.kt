32c2bd565bead91c4c0957b01a308c64f0bec77a
Nasalevich_MA_16 <nasalevich_ma_16@mf.grsu.by>
2019-03-29 22:08:12 +0300

    added text font & home menu

--- app/src/main/java/com/example/epamandroid/views/MainActivity.kt
+++ app/src/main/java/com/example/epamandroid/views/MainActivity.kt
@@ -2,16 +2,22 @@ package com.example.epamandroid.views
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
 import com.example.epamandroid.R
-import kotlinx.android.synthetic.main.custom_action_bar_layout.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(customActionBarLayout)
+        setSupportActionBar(activityMainCustomActionBarLayout as Toolbar?)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_action_bar_menu, menu)
 
+        return super.onCreateOptionsMenu(menu)
+    }
 }
