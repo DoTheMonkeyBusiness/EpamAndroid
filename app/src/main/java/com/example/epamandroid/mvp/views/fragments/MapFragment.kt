2c11a7fd6801afe32f4396ea000a06e12f4d0b6e
Nasalevich_MA_16 <nasalevich_ma_16@mf.grsu.by>
2019-05-12 23:20:35 +0300

    added device location

--- app/src/main/java/com/example/epamandroid/mvp/views/fragments/MapFragment.kt
+++ app/src/main/java/com/example/epamandroid/mvp/views/fragments/MapFragment.kt
@@ -2,19 +2,24 @@ package com.example.epamandroid.mvp.views.fragments
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.epamandroid.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapsInitializer
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 
 
@@ -22,11 +27,13 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     companion object {
         private const val TAG: String = "MapFragment"
+        private const val DEFAULT_ZOOM_KEY: Float = 15F
         private const val LOCATION_PERMISSION_KEY: Int = 1778
     }
 
     private var lostDogsMap: GoogleMap? = null
     private var locationPermissionsGranted: Boolean = false
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.map_fragment, container, false)
@@ -38,13 +45,13 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         getLocationPermission()
     }
 
-    private fun initMap(){
+    private fun initMap() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.mapFragmentMapView) as SupportMapFragment
 
         mapFragment.getMapAsync(this@MapFragment)
     }
 
-    private fun getLocationPermission(){
+    private fun getLocationPermission() {
         val permissions: Array<String> = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
 
         if (context?.let { ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) } == PackageManager.PERMISSION_DENIED
@@ -53,15 +60,16 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             requestPermissions(permissions, LOCATION_PERMISSION_KEY)
         } else {
             locationPermissionsGranted = true
+            initMap()
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-       locationPermissionsGranted = false
+        locationPermissionsGranted = false
 
-        when(requestCode) {
+        when (requestCode) {
             LOCATION_PERMISSION_KEY -> {
-                if (grantResults.isNotEmpty() && grantResults.all{ it == PackageManager.PERMISSION_GRANTED}){
+                if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
                     locationPermissionsGranted = true
                     initMap()
                 } else {
@@ -72,8 +80,44 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     }
 
+    private fun getDeviceLocation() {
+        fusedLocationProviderClient = context?.let { LocationServices.getFusedLocationProviderClient(it) }
+
+        try {
+            if (locationPermissionsGranted) {
+                val location = fusedLocationProviderClient?.lastLocation
+                location?.addOnCompleteListener{
+                    if (it.isSuccessful){
+                        val currentLocation = it.result as Location
+
+                        moveCamera(LatLng(currentLocation.latitude, currentLocation.longitude), DEFAULT_ZOOM_KEY)
+                    } else {
+                        Log.d(TAG, "current location is null")
+                        Toast.makeText(context, getString(R.string.unable_location), Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e(TAG, e.message)
+        }
+    }
+
+    private fun moveCamera(latLng: LatLng, zoom: Float) {
+        lostDogsMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom))
+    }
+
     override fun onMapReady(map: GoogleMap?) {
         lostDogsMap = map
 
+        if(locationPermissionsGranted){
+            getDeviceLocation()
+
+            if (context?.let { ActivityCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) } != PackageManager.PERMISSION_GRANTED
+                    && context?.let { ActivityCompat.checkSelfPermission(it, Manifest.permission.ACCESS_COARSE_LOCATION) } != PackageManager.PERMISSION_GRANTED
+            ){
+                return
+            }
+            lostDogsMap?.isMyLocationEnabled = true
+        }
     }
 }
