647a892e074bc73b0f27c303e49614f96e3800ed
Maksim Nasalevich <maksim_nasalevich@epam.com>
2020-06-16 10:24:50 +0300

    changed views

--- app/src/main/java/com/example/epamandroid/constants/URLConstants.kt
+++ app/src/main/java/com/example/epamandroid/constants/URLConstants.kt
@@ -2,8 +2,8 @@ package com.example.epamandroid.constants
 
 object URLConstants {
      const val RESPONSE_TIME_EXTRA_KEY: Long = 60
-     const val DOG_BREEDS_URL_STRING_EXTRA_KEY = "https://dogbreeds-60b2e.firebaseio.com/dogBreeds.json"
-     const val NOT_MODERATED_LOST_DOGS_URL_STRING_EXTRA_KEY = "https://dogbreeds-60b2e.firebaseio.com/notModaratedLostDogs.json"
+     const val DOG_BREEDS_URL_STRING_EXTRA_KEY = "https://university-59832.firebaseio.com/dogBreeds.json"
+     const val NOT_MODERATED_LOST_DOGS_URL_STRING_EXTRA_KEY = "https://university-59832.firebaseio.com/notModaratedLostDogs.json"
      const val MODERATED_LOST_DOGS_URL_STRING_EXTRA_KEY = "https://dogbreeds-60b2e.firebaseio.com/notModaratedLostDogs.json"
      const val STORAGE_LOST_DOGS_URL_STRING_EXTRA_KEY = "https://firebasestorage.googleapis.com/v0/b/dogbreeds-60b2e.appspot.com/o/lostDogs%2F"
      const val ALT_MEDIA_EXTRA_KEY = "?alt=media"
